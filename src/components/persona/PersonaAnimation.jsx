024442a42d2bcae499eae54c3ce35e0e42a260ad
Dipak Gohil <dipak@simformsolutions.com>
2024-09-05 19:57:08 +0530

    animation in progress

--- src/components/persona/PersonaAnimation.jsx
+++ src/components/persona/PersonaAnimation.jsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import gsap from "gsap";
 import "./PersonaAnimation.scss";
 
@@ -6,13 +6,12 @@ function PersonaAnimation() {
   const cardRef1 = useRef(null);
   const cardRef2 = useRef(null);
   const cardRef3 = useRef(null);
-  const cardRef4 = useRef(null);
-  const cardRef5 = useRef(null);
   const descRef = useRef(null);
   const gradientRef = useRef(null);
 
+  const [currentStep, setCurrentStep] = useState(1);
+
   useEffect(() => {
-    // Create timelines for the card animations
     const tl1 = gsap.timeline({
       defaults: { ease: "power4", duration: 0.8 },
     });
@@ -23,117 +22,208 @@ function PersonaAnimation() {
     tl1
       .fromTo(
         cardRef1.current,
-        { opacity: 0, y: 220, x: -15, scale: 0.8, rotate: -10 },
-        { opacity: 1, y: -100, x: 15, rotate: 0, scale: 1, duration: 0.7 },
+        { y: 200, x: -100, scale: 0.8 },
+        { y: -20, x: 10, duration: 0.6, scale: 1 },
         0
       )
       .fromTo(
         cardRef2.current,
-        { opacity: 0, y: 200, x: -10, scale: 0.8 },
-        { opacity: 1, y: -100, x: 10, duration: 0.6, scale: 1 },
+        { y: 150, x: 0, scale: 0.8 },
+        { y: -20, x: 0, duration: 0.65, scale: 1 },
         0
       )
       .fromTo(
         cardRef3.current,
-        { opacity: 0, y: 150, x: 0, scale: 0.8 },
-        { opacity: 1, y: -100, x: 0, duration: 0.65, scale: 1 },
-        0
-      )
-      .fromTo(
-        cardRef4.current,
-        { opacity: 0, y: 100, x: 10, scale: 0.8 },
-        { opacity: 1, y: -100, x: -10, duration: 0.6, scale: 1 },
-        0
-      )
-      .fromTo(
-        cardRef5.current,
-        { opacity: 0, y: 100, x: 15, rotate: 10, scale: 0.8 },
-        { opacity: 1, y: -100, x: -15, rotate: 0, duration: 0.5, scale: 1 },
+        { y: 100, x: 100, scale: 0.8 },
+        { y: -20, x: -10, duration: 0.6, scale: 1 },
         0
       );
 
     tl2
-      .to(cardRef3.current, { y: -65, scale: 1.1, duration: 0.6 }, 1)
-      .to(cardRef1.current, { rotate: -4, x: -15, y: 10, duration: 0.62 }, 1)
-      .to(cardRef2.current, { rotate: -2, x: -10, y: -20, duration: 0.69 }, 1)
-      .to(cardRef4.current, { rotate: 2, x: 10, y: -20, duration: 0.69 }, 1)
-      .to(cardRef5.current, { rotate: 4, x: 10, y: 10, duration: 0.62 }, 1)
       .fromTo(
         descRef.current,
         { y: 70, opacity: 0, scale: 0.5 },
-        { y: -40, opacity: 1, scale: 1, duration: 0.6 },
-        1
+        { y: -50, opacity: 1, scale: 1, duration: 0.6 },
+        0.7
       )
-      .to(
-        gradientRef.current,
-        {
-          y: -100,
-        },
-        1
-      );
+      .to(gradientRef.current, { y: -100 }, 0.7);
   }, []);
+
+  useEffect(() => {
+    // Apply the blur and opacity effect based on the current step
+    gsap.to([cardRef1.current, cardRef2.current, cardRef3.current], {
+      filter: "blur(1px)",
+      opacity: 0.5,
+      duration: 0.5,
+      ease: "power4.out",
+      pointerEvents: "none",
+      userSelect: "none",
+    });
+
+    // Highlight the current step
+    gsap.to(
+      currentStep === 1
+        ? cardRef1.current
+        : currentStep === 2
+        ? cardRef2.current
+        : cardRef3.current,
+      {
+        opacity: 1,
+        filter: "none",
+        scale: 1,
+        duration: 0.5,
+        ease: "power4.out",
+        pointerEvents: "all",
+      }
+    );
+  }, [currentStep]);
+
+  const handleNextStep = () => {
+    if (currentStep < 3) {
+      setCurrentStep((prev) => prev + 1);
+    }
+  };
 
   return (
     <div className="container">
       <div className="cards-wrapper">
-        <div className="card" ref={cardRef1}>
-          <div className="card-wrapper">
-            <div className="card-wrapper-acounts">
-              <div className="card-score">1</div>
-            </div>
-          </div>
-          <div className="card-title">Web Design templates Selection</div>
-          <div className="card-subtitle">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elitsed do
-            eiusmod.
-          </div>
-        </div>
-        <div className="card" ref={cardRef2}>
-          <div className="card-wrapper">
-            <div className="card-wrapper-acounts">
-              <div className="card-score">2</div>
-            </div>
-          </div>
-          <div className="card-title">Web Design templates Selection</div>
-          <div className="card-subtitle">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elitsed do
-            eiusmod.
-          </div>
-        </div>
-        <div className="card" ref={cardRef3}>
-          <div className="card-wrapper">
-            <div className="card-wrapper-acounts">
-              <div className="card-score">3</div>
-            </div>
-          </div>
-          <div className="card-title">Web Design templates Selection</div>
-          <div className="card-subtitle">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elitsed do
-            eiusmod.
-          </div>
-        </div>
-        <div className="card" ref={cardRef4}>
-          <div className="card-wrapper">
-            <div className="card-wrapper-acounts">
-              <div className="card-score">4</div>
-            </div>
-          </div>
-          <div className="card-title">Web Design templates Selection</div>
-          <div className="card-subtitle">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elitsed do
-            eiusmod.
+        <div ref={cardRef1}>
+          <div className="form-container">
+            <p className="title">Signup Info</p>
+            <form
+              className="form"
+              onSubmit={(e) => {
+                e.preventDefault();
+                handleNextStep();
+              }}
+            >
+              <div className="input-group">
+                <label htmlFor="email">Email Address</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Enter email address"
+                />
+              </div>
+              <div className="input-group">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Enter password"
+                />
+              </div>
+              <div className="input-group">
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <input
+                  type="password"
+                  name="confirmPassword"
+                  id="confirmPassword"
+                  placeholder="Enter password again"
+                />
+              </div>
+              <button className="form-btn" type="submit">
+                Next
+              </button>
+            </form>
           </div>
         </div>
-        <div className="card" ref={cardRef5}>
-          <div className="card-wrapper">
-            <div className="card-wrapper-acounts">
-              <div className="card-score">5</div>
-            </div>
+        <div ref={cardRef2}>
+          <div className="form-container">
+            <p className="title">Personal Info</p>
+            <form
+              className="form"
+              onSubmit={(e) => {
+                e.preventDefault();
+                handleNextStep();
+              }}
+            >
+              <div className="input-group">
+                <label htmlFor="email">Email Address</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Enter email address"
+                />
+              </div>
+              <div className="input-group">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Enter password"
+                />
+              </div>
+              <div className="input-group">
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <input
+                  type="password"
+                  name="confirmPassword"
+                  id="confirmPassword"
+                  placeholder="Enter password again"
+                />
+              </div>
+              <div className="btn-wrapper">
+                <button className="back-btn" type="submit">
+                  Back
+                </button>
+                <button className="form-btn" type="submit">
+                  Next
+                </button>
+              </div>
+            </form>
           </div>
-          <div className="card-title">Web Design templates Selection</div>
-          <div className="card-subtitle">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elitsed do
-            eiusmod.
+        </div>
+        <div ref={cardRef3}>
+          <div className="form-container">
+            <p className="title">Professional Info</p>
+            <form
+              className="form"
+              onSubmit={(e) => {
+                e.preventDefault();
+                handleNextStep();
+              }}
+            >
+              <div className="input-group">
+                <label htmlFor="email">Email Address</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Enter email address"
+                />
+              </div>
+              <div className="input-group">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Enter password"
+                />
+              </div>
+              <div className="input-group">
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <input
+                  type="password"
+                  name="confirmPassword"
+                  id="confirmPassword"
+                  placeholder="Enter password again"
+                />
+              </div>
+              <div className="btn-wrapper">
+                <button className="back-btn" type="submit">
+                  Back
+                </button>
+                <button className="form-btn" type="submit">
+                  Finish
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
