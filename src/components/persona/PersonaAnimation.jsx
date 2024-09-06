ffe9d746b6fbc2371c40a9a0fbe98c037eb89fce
Dipak Gohil <dipak@simformsolutions.com>
2024-09-06 16:01:46 +0530

    animation done

--- src/components/persona/PersonaAnimation.jsx
+++ src/components/persona/PersonaAnimation.jsx
@@ -13,53 +13,51 @@ function PersonaAnimation() {
 
   useEffect(() => {
     const tl1 = gsap.timeline({
-      defaults: { ease: "power4", duration: 0.8 },
+      defaults: { ease: "power4", duration: 1 },
     });
     const tl2 = gsap.timeline({
-      defaults: { ease: "power4", duration: 0.8 },
+      defaults: { ease: "power4", duration: 1 },
     });
-
     tl1
       .fromTo(
         cardRef1.current,
         { y: 200, x: -100, scale: 0.8 },
-        { y: -20, x: 10, duration: 0.6, scale: 1 },
+        { y: -20, x: 10, duration: 0.9, scale: 1 },
         0
       )
       .fromTo(
         cardRef2.current,
         { y: 150, x: 0, scale: 0.8 },
-        { y: -20, x: 0, duration: 0.65, scale: 1 },
+        { y: -20, x: 0, duration: 1, scale: 1 },
         0
       )
       .fromTo(
         cardRef3.current,
         { y: 100, x: 100, scale: 0.8 },
-        { y: -20, x: -10, duration: 0.6, scale: 1 },
+        { y: -20, x: -10, duration: 0.9, scale: 1 },
         0
       );
-
     tl2
       .fromTo(
         descRef.current,
-        { y: 70, opacity: 0, scale: 0.5 },
-        { y: -50, opacity: 1, scale: 1, duration: 0.6 },
+        { y: 70, opacity: 0 },
+        { y: -50, opacity: 1, duration: 0.9 },
         0.7
       )
-      .to(gradientRef.current, { y: -100 }, 0.7);
+      .to(gradientRef.current, { y: -100 }, 1);
   }, []);
 
   useEffect(() => {
     // Apply the blur and opacity effect based on the current step
     gsap.to([cardRef1.current, cardRef2.current, cardRef3.current], {
       filter: "blur(1px)",
-      opacity: 0.5,
+      opacity: 0.7,
       duration: 0.5,
       ease: "power4.out",
       pointerEvents: "none",
       userSelect: "none",
+      boxShadow: "none",
     });
-
     // Highlight the current step
     gsap.to(
       currentStep === 1
@@ -74,6 +72,8 @@ function PersonaAnimation() {
         duration: 0.5,
         ease: "power4.out",
         pointerEvents: "all",
+        boxShadow:
+          "rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px",
       }
     );
   }, [currentStep]);
@@ -84,10 +84,16 @@ function PersonaAnimation() {
     }
   };
 
+  const handlePreviousStep = () => {
+    if (currentStep > 1) {
+      setCurrentStep((prev) => prev - 1);
+    }
+  };
+
   return (
     <div className="container">
       <div className="cards-wrapper">
-        <div ref={cardRef1}>
+        <div ref={cardRef1} className="box">
           <div className="form-container">
             <p className="title">Signup Info</p>
             <form
@@ -124,13 +130,16 @@ function PersonaAnimation() {
                   placeholder="Enter password again"
                 />
               </div>
-              <button className="form-btn" type="submit">
-                Next
-              </button>
+              <div className="btn-wrapper">
+                <button className="form-btn" type="submit">
+                  Next
+                </button>
+              </div>
             </form>
           </div>
         </div>
-        <div ref={cardRef2}>
+
+        <div ref={cardRef2} className="box">
           <div className="form-container">
             <p className="title">Personal Info</p>
             <form
@@ -168,7 +177,11 @@ function PersonaAnimation() {
                 />
               </div>
               <div className="btn-wrapper">
-                <button className="back-btn" type="submit">
+                <button
+                  className="form-btn back-btn"
+                  type="button"
+                  onClick={handlePreviousStep}
+                >
                   Back
                 </button>
                 <button className="form-btn" type="submit">
@@ -178,14 +191,16 @@ function PersonaAnimation() {
             </form>
           </div>
         </div>
-        <div ref={cardRef3}>
+
+        <div ref={cardRef3} className="box">
           <div className="form-container">
             <p className="title">Professional Info</p>
             <form
               className="form"
               onSubmit={(e) => {
                 e.preventDefault();
-                handleNextStep();
+                // Final step
+                console.log("Form submission completed!");
               }}
             >
               <div className="input-group">
@@ -216,7 +231,11 @@ function PersonaAnimation() {
                 />
               </div>
               <div className="btn-wrapper">
-                <button className="back-btn" type="submit">
+                <button
+                  className="form-btn back-btn"
+                  type="button"
+                  onClick={handlePreviousStep}
+                >
                   Back
                 </button>
                 <button className="form-btn" type="submit">
@@ -229,10 +248,10 @@ function PersonaAnimation() {
       </div>
 
       <div className="desc-wrapper" ref={descRef}>
-        <h1 className="title">Choose your persona</h1>
+        <h1 className="title">Complete Your Profile</h1>
         <p className="desc">
-          Select a persona that best represents you. Customize your experience
-          to match your style and preferences.
+          Provide your account, personal, and professional details to create a
+          customized experience tailored to your needs.
         </p>
         <button>
           Let&apos;s Go
